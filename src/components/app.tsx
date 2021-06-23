02161d71a41e88d87684f1308362606199dc59fd
Jeffery Bennett <meandmybadself@gmail.com>
2021-06-22 21:26:52 -0500

    Updates docs

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -4,9 +4,11 @@ import CookieInput from './cookie-text-input'
 const App: React.FunctionComponent = () => {
     const [email, setEmail] = useState<string>('')
     return (<div id='app'>
-        <h1>React Cookie Input</h1>
+        <h1>🍪<br />React Cookie Input</h1>
         <p>Enter text in the input field below &amp; refresh.  The value should persist.</p>
         <CookieInput label='Email' value={email} updater={(value: string) => { setEmail(value); }} placeholder="Email" cookieKey="email" />
+        <hr />
+        <a href='https://github.com/meandmybadself/react-cookie-input'>Github</a>
     </div>)
 }
 
